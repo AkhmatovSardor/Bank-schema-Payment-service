f4c55841f9f746a84dec8d587c1c70961cdf1b89
AkhmatovSardor <sardor.akhmatov0969@gmail.com>
2023-08-28 14:39:55 +0500

    first commit

---
+++ src/test/java/com/company/Bankpaymentservice/BankPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Bankpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
